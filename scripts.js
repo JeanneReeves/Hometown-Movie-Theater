69cf4990b865a35c9924e0ee084cb50c6aeccb31
Jeanne Reeves <jeanne.reeves@student.swic.edu>
2019-10-20 23:40:23 -0500

    Get elements from page and log values.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
-// Enter a description of this program here.
+// A simple program for totaling and tracking ticket purchases
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+// Global functions inside Object Literal making them methods
+const tickets = {
+  multiply(a, b, c) {
+    return a * b * c;
+  }
+};
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const operation = document.querySelector("#operation");
+const select = document.querySelector("select");
+const output = document.querySelector("output");
+let ticketTotal = 0;
+
+// Use an anonymous callback function as the 2nd argument
+// What do we want to listen for and what do we want to do
+form.addEventListener("submit", function(event) {
+  // Prevent the default browser behavior - Don't submit
+  event.preventDefault();
+
+  // Form inputs have a property called value
+  const ticket = tickets.multiply(num1.value, num2.value, operation.value);
+
+  // Totaling of tickets
+  ticketTotal = ticket + ticketTotal;
+
+  output.textContent = ticketTotal;
+
+  //w3schools.com/js/js_htmldom_nodes.asp
+  // Adding receipts of transactions
+  const para = document.createElement("p");
+  const node = document.createTextNode(
+    "Number of tickets: " +
+      num1.value +
+      ", Price of tickets: " +
+      num2.value +
+      ", Discount: " +
+      operation.value +
+      ", Total: " +
+      tickets.multiply(num1.value, num2.value, operation.value)
+  );
+  para.appendChild(node);
+  const element = document.getElementById("ticketHistory");
+  element.appendChild(para);
+});
